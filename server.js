d871a0e19283246af9eb4c8f1032469755b5166e
Wyatt C <wcastellan@yahoo.com>
2021-12-12 21:49:21 -0500

    edit server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const { animals } = require('./data/animals.json');
 const express = require('express')
-const PORT = process.env.PORT || 3001;
+const { animals } = require('./data/animals.json');
 
+const PORT = process.env.PORT || 3001;
 const app = express()
 
 function filterByQuery(query, animalsArray) {
